bc6e1aa43a4426bee684b2a910bf68a1d05199cf
Pedro Emilio <pedro@rckt.com.br>
2018-03-24 18:08:25 -0300

    Sketch para 1

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
+  createCanvas(400,400);
   background(0);
 }
 
 function draw() {
+  //
+}
 
+function mouseDragged() {
+  fill(15, 127, 127);
+  noStroke();
+  ellipse(mouseX, mouseY, 10, 10);
 }
